7ee567a2b7b827d7ea20da4de2f9e6c94b75318f
rustem <kaidohmary@gmail.com>
2023-05-16 21:34:51 +0300

    add dequeue method

--- src/queue.py
+++ src/queue.py
@@ -1,3 +1,4 @@
+
 class Node:
     """Класс для узла очереди"""
 
@@ -19,6 +20,19 @@ class Queue:
         self.tail = None
         self.head = None
 
+    def __str__(self):
+        """Магический метод для строкового представления объекта"""
+        head = self.head
+        string_data = ''
+        while head:
+            if head.next_node:
+                string_data += str(head.data) + '\n'
+            else:
+                string_data += str(head.data)
+            head = head.next_node
+
+        return string_data
+
     def enqueue(self, data):
         """
         Метод для добавления элемента в очередь
@@ -33,26 +47,18 @@ class Queue:
             self.tail.next_node = new_node
             self.tail = new_node
 
-
-
-
     def dequeue(self):
         """
         Метод для удаления элемента из очереди. Возвращает данные удаленного элемента
 
         :return: данные удаленного элемента
         """
-        pass
+        if self.head is None:
+            return None
 
-    def __str__(self):
-        """Магический метод для строкового представления объекта"""
         head = self.head
-        string_data = ''
-        while head:
-            if head.next_node:
-                string_data += str(head.data) + '\n'
-            else:
-                string_data += str(head.data)
-            head = head.next_node
+        self.head = self.head.next_node
+        return head.data
 
-        return string_data
+
+
