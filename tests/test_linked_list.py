2789e1896e9da450ebf0874b1573adbd33014f5b
Memorizu <kaidohmary@gmail.com>
2023-05-28 16:27:14 +0300

    added tests, homework6 complete

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -26,6 +26,20 @@ class TestLinkedList(unittest.TestCase):
         ll.insert_at_end({'id': 3})
         self.assertEqual(str(ll).strip(), "{'id': 2} -> {'id': 3} -> None")
 
+    def test_to_list(self):
+        ll = LinkedList()
+        ll.insert_at_end({'id': 2})
+        ll.insert_at_end({'id': 3})
+        ll.insert_beginning({'id': 1})
+        self.assertEqual(ll.to_list(), [{'id': 1}, {'id': 2}, {'id': 3}])
+
+    def test_get_data_by_id(self):
+        ll = LinkedList()
+        ll.insert_at_end({'id': 2})
+        ll.insert_at_end({'id': 3})
+        ll.insert_beginning({'id': 1})
+        self.assertEqual(ll.get_data_by_id(1), {'id': 1})
+
 
 if __name__ == '__main__':
     unittest.main()
